0c11b37b679441e1e8ed030714c41fc7af7daa5b
Victor <mhpd100.i@gmail.com>
2023-12-18 01:28:41 +0200

    React base level: #1 Practice - practiced props

---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+
+    const data = [
+        {name: 'Syniak A.', salary: 2000, increase: false},
+        {name: 'Bohdanova T.', salary: 3000, increase: true},
+        {name: 'Shafranov V.', salary: 800, increase: false}
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+            </div>
+
+            <EmployeesList data={data} />
+            <EmployeesAddForm />
+        </div>
+    );
+}
+
+export default App;
